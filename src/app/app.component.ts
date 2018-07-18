d959ebdc2f702074ce0d60c4083edff7a5f92e2c
Oliver Favell <OliverFavell@etech.net>
2018-07-18 13:01:29 +0100

    Implemented bare features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sf';
+
+  public baitList = [
+    {id: 1, name: 'Lil\' Worm'},
+    {id: 2, name: 'Firefly'},
+    {id: 3, name: 'Grasshopper'},
+    {id: 4, name: 'Maggot'},
+    {id: 5, name: 'Shark meat'},
+  ];
+
+  private fishList = [
+    {id: 1, color: 'red'},
+    {id: 2, color: 'blue'},
+    {id: 3, color: 'green'},
+    {id: 4, color: 'yellow'},
+    {id: 5, color: 'orange'},
+  ];
+
+  public casted = false;
+  public selected;
+
+  public fish = null;
+
+  private caughtList = []
+
+  constructor() { }
+
+  castLine() {
+    this.casted = true;
+    this.startFishing();
+  }
+
+  catchFish() {
+    this.caughtList.push(this.fish);
+    alert(`You've caught ${this.fish.id}`);
+    this.fish = null;
+  }
+
+  startFishing() {
+    const timer = Math.floor(Math.random() * 32 * 1000);
+    console.log(timer); // remove once dev done
+    setTimeout(() => {
+      this.fish = this.fishList[Math.floor(Math.random() * this.fishList.length)];
+      this.casted = false;
+    }, timer);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
 
@@ -7,9 +11,15 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
